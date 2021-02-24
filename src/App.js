0d83489a890d71970183e2a62b040310dbdf3612
Byron Chang <byron_chang@hotmail.com>
2021-02-24 07:31:19 -0600

    Modifying Styles and App main.
    Modifying styles and app main file and index.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Router, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Home from "./components/Home";
+import { history } from "./helpers/history";
+import * as Actions from "./actions";
+import CoverAlbum from "./components/CoverAlbum";
 
-function App() {
+const App = () => {  
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(Actions.getAllBreeds());
+    // eslint-disable-next-line
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <div> 
+      <nav className="navbar fixed-top navbar-dark bg-dark">
+        <span className="navbar-brand mb-0 h1">Dog´s Album</span>
+      </nav>
+        <div>         
+          <Switch>
+            <Route exact path={["/", "/home"]} component={Home} />
+            <Route exact path={["/album"]} component={CoverAlbum} />
+            <Route exact path={["/album/:breed"]} component={CoverAlbum} />
+            <Route exact path={["/album/:breed/:subbreed"]} component={CoverAlbum} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
